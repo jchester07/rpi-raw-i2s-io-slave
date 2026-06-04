3d4dc2525f99f0639b5eed98eb42601e7b028c29
jchester07 <50397941+jchester07@users.noreply.github.com>
2026-06-04 15:28:49 +0800

    Remove non_legacy_dai_naming from codec driver

--- rpi-raw-i2s-codec.c
+++ rpi-raw-i2s-codec.c
@@ -38,7 +38,6 @@ static const struct snd_soc_component_driver soc_component_dev_rpi_raw_i2s_codec
 	.idle_bias_on		= 1,
 	.use_pmdown_time	= 1,
 	.endianness		= 1,
-	.non_legacy_dai_naming	= 1,
 };
 
 static int rpi_raw_i2s_codec_soc_probe(struct platform_device *pdev)
