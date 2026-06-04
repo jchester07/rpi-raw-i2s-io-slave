3f801af7c22e1b3b125ac98c35f2458580c686ed
jchester07 <50397941+jchester07@users.noreply.github.com>
2026-06-04 15:21:04 +0800

    Fix function name from asoc_rtd_to_cpu to snd_soc_rtd_to_cpu
    Using new kernel 6.12.47

--- rpi-simple-soundcard-mod.c
+++ rpi-simple-soundcard-mod.c
@@ -60,7 +60,7 @@ static int snd_rpi_simple_mod_init(struct snd_soc_pcm_runtime *rtd)
 {
 	struct snd_rpi_simple_mod_drvdata *drvdata =
 		snd_soc_card_get_drvdata(rtd->card);
-	struct snd_soc_dai *cpu_dai = asoc_rtd_to_cpu(rtd, 0);
+	struct snd_soc_dai *cpu_dai = snd_soc_rtd_to_cpu(rtd, 0);
 
 	if (drvdata->fixed_bclk_ratio > 0)
 		return snd_soc_dai_set_bclk_ratio(cpu_dai,
@@ -73,7 +73,7 @@ static int snd_rpi_simple_mod_hw_params(struct snd_pcm_substream *substream,
 		struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *cpu_dai = asoc_rtd_to_cpu(rtd, 0);
+	struct snd_soc_dai *cpu_dai = snd_soc_rtd_to_cpu(rtd, 0);
 	struct snd_rpi_simple_mod_drvdata *drvdata;
 	unsigned int sample_bits;
 
